c207ec211671063a1d4d43ed94ea7f3df876a2d0
Eduardo Hernández <jhernandezg1403@gmail.com>
2024-10-08 18:27:08 -0600

    Corrección sumade costo final

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 let cuenta = 0;
 let personas =0;
-
+let cuentaFinal= 0;
 function cursoSolicitado(){
     const curso = parseInt(prompt("Elige el ID del curso que deseas tomar, solo selecciona un curso, posteriormente podrás añadir más"));
     console.log(curso);
     if (!isNaN(curso) && (curso <= 9) && (curso >= 1)) {
         if((curso == 1) || (curso == 4) || (curso == 7)){
             precioCurso = parseInt(450);
-            cuenta += precioCurso;
+            cuenta = precioCurso;
         } else {
             precioCurso = parseInt(300);
-            cuenta += precioCurso;
+            cuenta = precioCurso;
         }
             const personas = parseInt(prompt("¿Cuántas personas tomarán el curso? (Máximo 30 personas por curso)"));
             console.log(personas)
             if (!isNaN(personas) && (personas <= 30) && (personas >= 1)) {
                 console.log(personas)
                 console.log(cuenta)
-                cuenta = (personas*cuenta);
-                alert(`Tu cuenta al momento es de $${cuenta}`);
+                cuentaFinal += (personas*cuenta);
+                alert(`Tu cuenta al momento es de $${cuentaFinal}`);
                 if (personas >=30) {
                     alert("El número máximo de personas por grupo es de 30");
                 } 
@@ -39,5 +39,5 @@ while(agregar){
     agregar = confirm("¿Quires añadir más cursos?")
 }
 
-alert(`Tu cuenta final es de $${cuenta}`)
+alert(`Tu cuenta final es de $${cuentaFinal}`);
 
