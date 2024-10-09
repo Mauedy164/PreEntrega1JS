0cefe208b57267f01cd8d449606d17efc69350f0
Eduardo Hernández <jhernandezg1403@gmail.com>
2024-10-08 18:06:01 -0600

    Añadido contador de personas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let cuenta = 0;
+let personas =0;
 
 function cursoSolicitado(){
     const curso = parseInt(prompt("Elige el ID del curso que deseas tomar, solo selecciona un curso, posteriormente podrás añadir más"));
@@ -11,7 +12,21 @@ function cursoSolicitado(){
             precioCurso = parseInt(300);
             cuenta += precioCurso;
         }
-        alert(`Tu cuenta al momento es de $${cuenta}`)
+            const personas = parseInt(prompt("¿Cuántas personas tomarán el curso? (Máximo 30 personas por curso)"));
+            console.log(personas)
+            if (!isNaN(personas) && (personas <= 30) && (personas >= 1)) {
+                console.log(personas)
+                console.log(cuenta)
+                cuenta = (personas*cuenta);
+                alert(`Tu cuenta al momento es de $${cuenta}`);
+                if (personas >=30) {
+                    alert("El número máximo de personas por grupo es de 30");
+                } 
+            } else {
+                alert("Ingresa un número válido de personas");
+            }
+
+
     } else {
         alert("Ingresa un ID correcto")
     }
@@ -19,7 +34,8 @@ function cursoSolicitado(){
 let agregar = confirm("¿Quires añadir más cursos?");
 
 while(agregar){
-    cursoSolicitado();   
+    cursoSolicitado(); 
+      
     agregar = confirm("¿Quires añadir más cursos?")
 }
 
